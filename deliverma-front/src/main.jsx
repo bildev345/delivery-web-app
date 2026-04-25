5a43ba4ed016832c40792643a89cd253abd10591
Bilal el <bilalowraiylegh@gmail.com>
2026-04-25 12:49:17 +0100

    first commit

---
+++ deliverma-front/src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import AppRouter from './router/AppRouter';
+import './styles/main.scss';
+import AuthProvider from './components/AuthProvider';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 1,
+            staleTime: 1000 * 60 * 5, // 5 minutes
+        },
+    },
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <AppRouter />
+            </AuthProvider>
+        </QueryClientProvider>
+    </React.StrictMode>
+);
